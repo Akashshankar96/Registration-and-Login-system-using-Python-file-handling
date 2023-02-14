15e87f72a5566f678bc8e0f67cdd3432cb4fbe25
Akash_sh_ar <117355351+Akashshankar96@users.noreply.github.com>
2023-02-14 19:27:56 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,61 +1,53 @@
-# Register
+import re
+import getpass
 
 def register():
-    # Get email/username
-    email = input("Enter your email/username: ")
+    while True:
+        username = input("Enter your email/username: ")
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", username):
+            print("Invalid email/username. Please try again.")
+            continue
+        
+        if username.count(".") == 1 and username.index(".") > username.index("@"):
+            password = getpass.getpass("Enter a password (5-16 characters long, with at least one uppercase, one lowercase, one digit, and one special character): ")
+            if not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,16}$", password):
+                print("Invalid password. Please try again.")
+                continue
+            
+            with open("users.txt", "a") as file:
+                file.write(f"{username},{password}\n")
+            
+            print("Registration successful!")
+            break
+        else:
+            print("Invalid email/username. Please try again.")
 
-    # Validate email/username
-    if "@" in email or "." in email:
-        print("Invalid email/username. Please try again.")
-        return register()
-
-    # Get password
-    password = input("Enter your password: ")
-
-    # Validate password
-    if len(password) < 5 or len(password) > 16:
-        print("Invalid password. Please try again.")
-        return register()
-    if not any(char.isdigit() for char in password):
-        print("Invalid password. Please include at least one digit.")
-        return register()
-    if not any(char.isupper() for char in password):
-        print("Invalid password. Please include at least one uppercase character.")
-        return register()
-    if not any(char.islower() for char in password):
-        print("Invalid password. Please include at least one lowercase character.")
-        return register()
-    if not any(not char.isalnum() for char in password):
-        print("Invalid password. Please include at least one special character.")
-        return register()
-
-    # Save email/username and password to file
-    with open("user_data.txt", "w") as file:
-        file.write(email + "," + password)
-    print("Registration successful!")
-
-
-# Login
 def login():
-    # Get email/username
-    email = input("Enter your email/username: ")
+    while True:
+        username = input("Enter your email/username: ")
+        password = getpass.getpass("Enter your password: ")
+        
+        with open("users.txt", "r") as file:
+            for line in file:
+                line = line.strip().split(",")
+                if line[0] == username and line[1] == password:
+                    print("Login successful!")
+                    return
+            else:
+                print("Invalid username or password.")
+                choice = input("Enter 1 to register, 2 to try again, or q to quit: ")
+                if choice == "1":
+                    register()
+                elif choice == "q":
+                    break
 
-    # Check if email/username exists in file
-    with open("user_data.txt", "r") as file:
-        data = file.read()
-        if email not in data:
-            print("Email/username not found. Please try again or register.")
-            return
-
-    # Get password
-    password = input("Enter your password: ")
-
-    # Check if password is correct
-    with open("user_data.txt", "r") as file:
-        data = file.read().split(",")
-        if data[1] != password:
-            print("Incorrect password. Please try again or use the 'forgot password' feature.")
-            return
-
-    # Login successful
-    print("Login successful!")
+while True:
+    choice = input("Enter 1 to register, 2 to login, or q to quit: ")
+    if choice == "1":
+        register()
+    elif choice == "2":
+        login()
+    elif choice == "q":
+        break
+    else:
+        print("Invalid choice.")
